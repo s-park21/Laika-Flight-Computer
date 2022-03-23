dbadb890af72c8a7c4f6cd19c7ba965b98c963be
Angus McLennan <amcl0006@student.monash.edu>
2022-03-23 15:11:16 +1100

    Added new version of FC and restructured folders

---
+++ V3/RangeTest.m
@@ -0,0 +1,21 @@
+clc; clear all; close all;
+
+RSSI = table2array(importdata("RSSI.mat"));
+timeMs = table2array(importdata("timeMs.mat"));
+
+for i = 2:length(RSSI)
+    if isnan(timeMs(i))
+        timeMs(i) = 0;
+    end
+    if timeMs < 20000
+        timeMs(i) = 0;      %% remove data before 20s
+    end
+end
+
+badPackets = timeMs==0;
+timeS = timeMs/1000;
+
+plot(timeS, RSSI, 'r.');
+hold on
+plot(timeS,-120*badPackets, 'kd')
+hold on
